f9ff731b0277e09e91003635908a8586acf9dd44
Tuấn Hưng <tuanhung.nguyen.1980@gmail.com>
2023-08-28 21:27:54 +0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'views/plan_screen.dart';
+void main() => runApp(MasterPlanApp());
+
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
 
--- lib/views/plan_screen.dart
+++ lib/views/plan_screen.dart
@@ -13,6 +13,66 @@ class _PlanScreenState extends State<PlanScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Master plan')),
+      body: _buildList(),
+      floatingActionButton: _buildAddTaskAction(),
+      );
+
+  }
+
+  Widget _buildAddTaskAction(){
+    return FloatingActionButton(
+      child: const Icon(Icons.add),
+      onPressed: (){
+        setState(() {
+          plan = Plan(
+            name: plan.name,
+            tasks: List<Task>.from(plan.tasks)
+              ..add(const Task()),
+          );
+        });
+      },
+    );
+  }
+  Widget _buildList(){
+    return ListView.builder(
+      itemCount: plan.tasks.length,
+        itemBuilder: (context,index) => _builTaskTile(plan.tasks[index], index),
+    );
+  }
+  Widget _builTaskTile(Task task, int index)  {
+    return ListTile(
+      leading: Checkbox(
+        value: task.complete,
+        onChanged: (selected) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: task.description,
+                  complete: selected ?? false,
+                )
+            );
+          });
+        },
+      ),
+      title: TextFormField(
+        initialValue: task.description,
+        onChanged: (text) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete
+                )
+            );
+          });
+        }
+      ),
+    );
   }
 }
