bdef6fc68051c4ae825135a493de6980167484f1
Tuấn Hưng <tuanhung.nguyen.1980@gmail.com>
2023-08-28 20:28:11 +0700

    First commit

---
+++ lib/models/plan.dart
@@ -0,0 +1,6 @@
+import 'task.dart';
+class Plan {
+  final String name;
+  final List<Task> tasks;
+  const Plan({this.name = '', this.tasks = const []});
+}
---
+++ lib/views/plan_screen.dart
@@ -0,0 +1,18 @@
+import '../models/data_layer.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  const PlanScreen({super.key});
+
+  @override
+  State<PlanScreen> createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  Plan plan = const Plan();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
